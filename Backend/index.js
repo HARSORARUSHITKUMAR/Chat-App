e607346347ce51ea21a7507564a484497b788eb7
Rushit Harsora <rushitharsora@gmail.com>
2024-10-19 23:56:33 +0530

    chat-app

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const { chats } = require('./data/data')
+
+
+const app = express();
+dotenv.config();
+
+
+app.get('/', (req, res) => {
+    res.send("Api is Running");
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 3001
+
+
+app.listen(3001, console.log(`Server is started on ${PORT}`))
