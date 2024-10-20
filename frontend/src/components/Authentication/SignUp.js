e9af9f7acf9a5bbf20746b464377749a97dd2099
Rushit Harsora <rushitharsora@gmail.com>
2024-10-20 13:12:25 +0530

    updates

--- frontend/src/components/Authentication/SignUp.js
+++ frontend/src/components/Authentication/SignUp.js
@@ -100,9 +100,9 @@ const Signup = () => {
         if (pics.type === "image/jpeg" || pics.type === "image/png") {
             const data = new FormData();
             data.append("file", pics);
-            data.append("upload_preset", "chat-app");
-            data.append("cloud_name", "piyushproj");
-            fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+            data.append("upload_preset", "Chat-App");
+            data.append("cloud_name", "dnynfivgn");
+            fetch("https://api.cloudinary.com/v1_1/dnynfivgn/image/upload", {
                 method: "post",
                 body: data,
             })
